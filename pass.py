2570bb95ec3747dcbb505d83e8729cdb412a7319
Evance23 <evancebarracks01@gmail.com>
2021-09-03 17:03:50 +0300

    Add instances created in main file

--- pass.py
+++ pass.py
@@ -2,7 +2,7 @@ from user import User
 # from credentials import Credentials
 
 def function():
-    print("Welcome to Pass-Lock")
+    print("Welcome to Bar-Lock")
 
 function()
 
@@ -28,39 +28,39 @@ def login_user(username,password):
     function that checks whether a user exist and then login the user in.
     """
   
-    check_user = Credentials.verify_user(username,password)
-    return check_user
-def create_new_credential(account,userName,password):
-    """
-    Function that creates new credentials for a given user account
-    """
-    new_credential = Credentials(account,userName,password)
-    return new_credential
-def save_credentials(credentials):
-    """
-    Function to save Credentials to the credentials list
-    """
-    credentials. save_details()
-def display_accounts_details():
-    """
-    Function that returns all the saved credential.
-    """
-    return Credentials.display_credentials()
+#     check_user = Credentials.verify_user(username,password)
+#     return check_user
+# def create_new_credential(account,userName,password):
+#     """
+#     Function that creates new credentials for a given user account
+#     """
+#     new_credential = Credentials(account,userName,password)
+#     return new_credential
+# def save_credentials(credentials):
+#     """
+#     Function to save Credentials to the credentials list
+#     """
+#     credentials. save_details()
+# def display_accounts_details():
+#     """
+#     Function that returns all the saved credential.
+#     """
+#     return Credentials.display_credentials()
 
-def delete_credential(credentials):
-    """
-    Function to delete a Credentials from credentials list
-    """
-    credentials.delete_credentials()
-def generate_Password():
-    '''
-    generates a random password for the user.
-    '''
-    auto_password=Credentials.generatePassword()
-    return auto_password
-def copy_password(account):
-    """
-    A funct that copies the password using the pyperclip framework
-    We import the framework then declare a function that copies the emails.
-    """
-    return Credentials.copy_password(account)
+# def delete_credential(credentials):
+#     """
+#     Function to delete a Credentials from credentials list
+#     """
+#     credentials.delete_credentials()
+# def generate_Password():
+#     '''
+#     generates a random password for the user.
+#     '''
+#     auto_password=Credentials.generatePassword()
+#     return auto_password
+# def copy_password(account):
+#     """
+#     A funct that copies the password using the pyperclip framework
+#     We import the framework then declare a function that copies the emails.
+#     """
+#     return Credentials.copy_password(account)
