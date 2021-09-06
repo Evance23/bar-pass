d5edca2ddbefbae52e0342eb1cf5837bc1c1b69c
Evance23 <evancebarracks01@gmail.com>
2021-09-06 12:35:02 +0300

    Add new funtions to the main file

--- pass.py
+++ pass.py
@@ -19,11 +19,57 @@ def display_user():
     Function to display existing user
     """
     return User.display_user()
+def login_user(username,password):
+    """
+    function that checks whether a user exist and then login the user in.
+    """
+  
+    check_user = Records.verify_user(username,password)
+    return check_user
 
+def create_new_record(account,userName,password):
+    """
+    Function that creates new records for a given user account
+    """
+    new_record = Records(account,userName,password)
+    return new_record
+def save_record(record):
+    """
+    Function to save records
+    """
+    record. save_details()
+def display_accounts_details():
+    """
+    Function that returns all the saved accounts
+    """
+    return Records.display_records()
+def delete_record(records):
+    """
+    Function to delete the records in the list
+    """
+    record.delete_record()
 
-
-
+def find_record(account):
+  
+    return Records.find_record(account)
+def check_records(account):
     
+    return Records.if_record_exist(account)
+
+def generate_password():
+    '''
+    generates a random password for the user.
+    '''
+    auto_password=Records.generate_password()
+    return auto_password
+
+def copy_password(account):
+
+    return Records.copy_password(account)
+
+
+
+
 def main():
     print('\n')
     print(" Welcome to Bar-Pass!!!")
